d70af29341d4da375048e0b2dad7b9b85d92627f
warddaraghmeh <warddaraghmeh99@gmail.com>
2026-05-21 14:12:27 +0300

    useEffect

--- src/pages/html.js
+++ src/pages/html.js
@@ -2,8 +2,18 @@ import { Helmet } from "react-helmet-async";
 import Footer from "../comp/footer";
 import Header from "../comp/header";
 import MainContent from "../comp/MainContent";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config";
 const Html = () => {
+  const navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  useEffect(() => {
+    if (!user && !loading) {
+      navigate("/signin");
+    }
+  }, [user, loading, navigate]);
   return (
     <>
       <Helmet>
--- src/pages/javascript.js
+++ src/pages/javascript.js
@@ -2,7 +2,18 @@ import Header from "../comp/header";
 import Footer from "../comp/footer";
 import MainContent from "../comp/MainContent";
 import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config";
 const JavaScript = () => {
+  const navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  useEffect(() => {
+    if (!user && !loading) {
+      navigate("/signin");
+    }
+  }, [user, loading, navigate]);
   return (
     <>
       <Helmet>
